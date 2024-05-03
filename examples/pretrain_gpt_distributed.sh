ae586888e716d7e48dc0905b9a5eb3a15008522b
MaCasK <MaCasK@stu.pku.edu.cn>
2024-05-03 18:22:29 +0800

    Runable Megatron without edit

--- examples/pretrain_gpt_distributed.sh
+++ examples/pretrain_gpt_distributed.sh
@@ -4,18 +4,18 @@
 
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 
-GPUS_PER_NODE=8
+GPUS_PER_NODE=4
 # Change for multinode config
 MASTER_ADDR=localhost
-MASTER_PORT=6000
+MASTER_PORT=21121
 NNODES=1
 NODE_RANK=0
 WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
 
-CHECKPOINT_PATH=<Specify path>
-VOCAB_FILE=<Specify path to file>/gpt2-vocab.json
-MERGE_FILE=<Specify path to file>/gpt2-merges.txt
-DATA_PATH=<Specify path and file prefix>_text_document
+CHECKPOINT_PATH=./checkpoints
+VOCAB_FILE=./data/gpt2-vocab.json
+MERGE_FILE=./data/gpt2-merges.txt
+DATA_PATH=./data/gpt2-enwiki_text_document
 
 DISTRIBUTED_ARGS="
     --nproc_per_node $GPUS_PER_NODE \
@@ -31,17 +31,21 @@ GPT_ARGS="
     --num-attention-heads 16 \
     --seq-length 1024 \
     --max-position-embeddings 1024 \
-    --micro-batch-size 8 \
-    --global-batch-size 64 \
+    --micro-batch-size 2 \
+    --global-batch-size 8 \
     --lr 0.00015 \
-    --train-iters 500000 \
+    --train-iters 300000 \
     --lr-decay-iters 320000 \
     --lr-decay-style cosine \
     --min-lr 1.0e-5 \
     --weight-decay 1e-2 \
     --lr-warmup-fraction .01 \
     --clip-grad 1.0 \
-    --fp16
+    --attention-softmax-in-fp32 \
+    --overlap-grad-reduce \
+    --use-distributed-optimizer \
+    --use-mcore-models \
+    --overlap-param-gather
 "
 
 DATA_ARGS="
@@ -58,10 +62,12 @@ OUTPUT_ARGS="
     --eval-iters 10
 "
 
+rm -r $CHECKPOINT_PATH
+
 torchrun $DISTRIBUTED_ARGS pretrain_gpt.py \
     $GPT_ARGS \
     $DATA_ARGS \
     $OUTPUT_ARGS \
     --distributed-backend nccl \
     --save $CHECKPOINT_PATH \
-    --load $CHECKPOINT_PATH
+    | tee ./logs/pretrain_gpt.log
